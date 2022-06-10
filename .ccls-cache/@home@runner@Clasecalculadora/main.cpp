d0b4a07a8a19cf1d81bb3d2d73c9f79f8d495fe6
Jose Erley <101919696+jerleymt@users.noreply.github.com>
2022-06-10 03:52:46 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Clasecalculadora/main.cpp
@@ -0,0 +1,15 @@
+#include "ope.h"
+
+int main(){
+    clase_operacion calculadora;
+    calculadora.set_numero1(5);
+    calculadora.set_numero2(25);
+    float n1 = calculadora.get_numero1();
+    float n2 = calculadora.get_numero2();
+    std::cout<<"Se obtuveron los numeros "<<calculadora.get_numero1()<<" y "<<calculadora.get_numero2()<<std::endl;    
+    std::cout<<n1 <<" + "<<n2<<" = "<<calculadora.suma()<<std::endl;
+    std::cout<<n1 <<" - "<<n2<<" = "<<calculadora.resta()<<std::endl;
+    std::cout<<n1 <<" x "<<n2<<" = "<<calculadora.multiplicacion()<<std::endl;
+    std::cout<<n1 <<" ÷ "<<n2<<" = "<<calculadora.divicion()<<std::endl;
+    return 0;
+}
